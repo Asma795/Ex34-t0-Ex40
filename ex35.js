5240b489bf327717fc5e658b763872a79ee02a6d
Asma Shahid <asmashahid795@gmail.com>
2024-03-09 15:22:08 +0500

    Add files via upload

---
+++ ex35.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let animals = ['Dog', 'Cat', 'Lion'];
+for (const animal of animals) {
+    console.log(`A ${animal} would be a great pet`);
+}
+console.log(`Any of those four legged creatures would be a great pet`);
---
+++ ex37.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function make__shirt1(size = "Large", message = "I love typescript") {
+    console.log(`I would like to own a ${size} shirt. It should say ${message}.`);
+}
+make__shirt1();
+function make__shirt2(size = "Medium", message = "I love typescript") {
+    console.log(`I would like to own a ${size} shirt. It should say ${message}.`);
+}
+make__shirt2();
